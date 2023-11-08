0515ed226e911c221c5de1b57fdce9a27a32bc57
KevinBarrera <kev97barrera@gmail.com>
2023-11-08 17:20:15 -0600

    Configuring sequelize

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,6 +1,11 @@
 import { Sequelize } from "sequelize";
-const DB_PATH = process.env.DB_PATH ?? "./credit-health.db";
+import * as dbConfig from "./db.config";
 
-const sequelize = new Sequelize(`sqlite:${DB_PATH}`);
+const sequelize = new Sequelize(dbConfig.DB_NAME, dbConfig.DB_USER, dbConfig.DB_PASSWORD, {
+  host: dbConfig.DB_HOST,
+  dialect: dbConfig.dialect,
+  port: Number(dbConfig.DB_PORT),
+  pool: dbConfig.pool
+});
 
 export default sequelize;
