bdfb02e6293b1b2941fd155e92d5090eb04f324c
KevinBarrera <kev97barrera@gmail.com>
2023-11-14 13:01:21 -0600

    Adding otp/verify route to verify otp

--- src/routes/otp.ts
+++ src/routes/otp.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { sendOtp } from "../controllers/otp.controller";
+import { sendOtp, verifyOtp } from "../controllers/otp.controller";
 import { validateSchema } from "../middlewares/schemaValidator";
-import { EmailContentSchema } from "../schemas/otp.schemas";
+import { EmailContentSchema, VerifyOtpSchema } from "../schemas/otp.schemas";
 
 const router = Router();
 
 router.post("/", validateSchema(EmailContentSchema), sendOtp);
+
+router.post("/verify", validateSchema(VerifyOtpSchema), verifyOtp);
 
 export { router };
