067025e0e3c2405a2f70e19f9d6dafb7e1210e38
KevinBarrera <kev97barrera@gmail.com>
2023-11-14 12:01:17 -0600

    Using middleware where needed

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth.controller";
+import { validateSchema } from "../middlewares/schemaValidator";
+import { LoginSchema, RegisterSchema } from "../schemas/auth.schemas";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/register", validateSchema(RegisterSchema), register);
 
-router.post("/login", login);
+router.post("/login", validateSchema(LoginSchema), login);
 
 export { router };
